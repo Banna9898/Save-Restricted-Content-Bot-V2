0ba5fee275b796c94b68ea7a6c6467640ae07698
Banna9898 <68413142+Banna9898@users.noreply.github.com>
2024-07-25 00:25:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "29141829"))
 API_HASH = getenv("API_HASH", "25020d00fbcfd406fc9979d24d761bff")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
+BOT_TOKEN = getenv("BOT_TOKEN", "6998826537:AAE24A88dHWSxFLFQ5jFYs2Sq8UwDPtasug")
+OWNER_ID = int(getenv("OWNER_ID", "5323404314"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002071932943"))
+FORCESUB = getenv("FORCESUB", "jayhind_675")
